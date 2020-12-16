071257d9fff69fb63de498d95c7dc5d6bfc6a0f7
markernst97 <markiervc@gmail.com>
2020-12-16 13:05:14 +0100

    16-12-2020

--- src/prototype/complete/Klaar.js
+++ src/prototype/complete/Klaar.js
@@ -5,12 +5,23 @@ import { onClickAnalytics } from '../../Analytics';
 export const Klaar = (props) => {
     const history = useHistory();
 
+    const getRightForm = () => {
+        if (localStorage.getItem('userType') && localStorage.getItem('userType') === 'inf'){
+            console.log('run inf link')
+            return 'https://forms.gle/X6UArFuNesJngAFV7';
+        }
+        else {
+            console.log('run normal people link')
+            return 'https://docs.google.com/forms/d/e/1FAIpQLSfDMY0ar78532S20pZV0iE10JtPnYUQcNz8sGU3oi1QVKOhwg/viewform?usp=sf_link'
+        }
+    }
+
     return <PrototypePage childClass='klaar'>
         <div className='d-flex flex-column h-100 justify-content-center align-items-center'>
             <span className='klaar mb-2 '>De test is klaar</span>
             <span className='klaar mb-5'>Bedankt!</span>
             <div className='d-flex flex-column align-items-center align-items-center'>
-            <a target='_blank' href='https://docs.google.com/forms/d/e/1FAIpQLSfDMY0ar78532S20pZV0iE10JtPnYUQcNz8sGU3oi1QVKOhwg/viewform?usp=sf_link'><Button onClick={onClickAnalytics(() => {}, 'Vragen formulier')} className='pbtn'>Naar vragen beantwoorden</Button></a>
+            <a target='_blank' href={getRightForm()}><Button onClick={onClickAnalytics(() => {}, 'Vragen formulier')} className='pbtn'>Naar vragen beantwoorden</Button></a>
             <Button onClick={onClickAnalytics(() => history.push('/prototype'), 'Terug naar begin')} className='btn-small-text mt-5'>Terug naar begin</Button>
             <Button onClick={onClickAnalytics(() => {localStorage.removeItem('storage'); history.push('/prototype')}, 'Voortgang wissen')} className='btn-small-text mt-2'>Voortgang wissen en terug naar begin</Button>
 
